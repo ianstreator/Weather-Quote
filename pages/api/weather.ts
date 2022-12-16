0a6ef4527797e27852d064000c94913ab1a723aa
Ian <istreator@yahoo.com>
2022-12-16 02:05:09 -0600

    added upstash/redis dependency, corrected type usage, code clean up

--- pages/api/weather.ts
+++ pages/api/weather.ts
@@ -1,64 +1,44 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-
-type Coords = {
-  lat: number;
-  lon: number;
-};
+import { Coords, Weather, CityWeatherData, Error } from "../../Types";
 
 interface ExtendedNextApiRequest extends NextApiRequest {
   body: Coords;
 }
 
-type Weather = {
-  current: {};
-  daily: {}[];
-  lat: number;
-  lon: number;
-  timezone: string;
-  timezone_offset: number;
-};
-
-type Data = {
-  city: string | undefined;
-  weather: Weather | undefined;
-};
-type Error = {
-  message: any;
-};
-
 const WEATHER_API_BASE_URL = "http://api.openweathermap.org/";
 const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
 
 export default async function handler(
-  req: ExtendedNextApiRequest,
-  res: NextApiResponse<Data | Error>
+  { body: { lat, lon } }: ExtendedNextApiRequest,
+  res: NextApiResponse<CityWeatherData | Error>
 ) {
-  const c = req.body;
-
-  const getCurrentCity = async (c: Coords): Promise<string> => {
+  const getCurrentCity = async () => {
     const res = await fetch(
-      `${WEATHER_API_BASE_URL}geo/1.0/reverse?lat=${c.lat}&lon=${c.lon}&appid=${WEATHER_API_KEY}`
+      `${WEATHER_API_BASE_URL}geo/1.0/reverse?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
     );
-    return (await res.json()).map(
-      (cityData: { name: string }): string => cityData.name
-    )[0];
+    const [{ name: city }] = (await res.json()) as { name: string }[];
+
+    return city;
   };
 
-  const getCurrentWeather = async (c: Coords) => {
+  const getCurrentWeather = async () => {
     const res = await fetch(
-      `${WEATHER_API_BASE_URL}data/2.5/onecall?lat=${c.lat}&lon=${c.lon}&appid=${WEATHER_API_KEY}&exclude=minutely,hourly,alerts&units=imperial`
+      `${WEATHER_API_BASE_URL}data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&exclude=minutely,hourly,alerts&units=imperial`
     );
     const weather = (await res.json()) as Promise<Weather>;
     return weather;
   };
 
   try {
-    const weatherData = await Promise.all([
-      getCurrentCity(c),
-      getCurrentWeather(c),
-    ]).then(([city, weather]: [string, Weather]) => ({ city, weather }));
-    res.status(200).send({ ...weatherData });
+    const cityWeatherData = (await Promise.all([
+      getCurrentCity(),
+      getCurrentWeather(),
+    ]).then(([city, weather]) => ({
+      city,
+      weather,
+    }))) as CityWeatherData;
+    res.status(200).send({ ...cityWeatherData });
   } catch (error) {
     res.status(500).json({ message: error });
   }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,71 +1,96 @@
-import { nextConfig } from "../next.config";
 import { useEffect, useState } from "react";
-import { SetStateAction } from "react";
+import { CityWeatherData, Coords, ServerSideAssets } from "../Types";
+import { nextConfig } from "../next.config";
+const { environment } = nextConfig;
 
-type WeatherData = {
-  city: string | undefined;
-  weather: {} | undefined;
-};
+import Image from "next/image";
 
-export default function Home() {
-  const [weatherData, setWeatherData] = useState<WeatherData>();
+import WeekDayCard from "../components/WeekDayCard";
+
+export default function Home({ url }: ServerSideAssets) {
+  const [cityWeatherData, setCityWeatherData] = useState<CityWeatherData>();
+
+  console.log(url);
 
   useEffect(() => {
-    async function getCoordinates() {
-      const pos: { coords: { latitude: number; longitude: number } } =
-        await new Promise((res, rej) => {
-          navigator.geolocation.getCurrentPosition(res, rej);
-        });
-      const location = {
-        lat: pos.coords.latitude,
-        lon: pos.coords.longitude,
-      };
-      return location;
-    }
-    getWeather(setWeatherData, getCoordinates);
+    (async () => {
+      const coords = await getCoordinates();
+      const cityWeatherData = await fetchCityWeatherData(coords);
+      setCityWeatherData(cityWeatherData);
+    })();
   }, []);
-  useEffect(() => {
-    console.log(weatherData);
-  }, [weatherData]);
 
-  if (!weatherData) return <div>Checking weather...</div>;
+  if (!cityWeatherData) return <div>Checking weather...</div>;
 
   return (
-    <div>
+    <main
+      style={{
+        backgroundImage: `url(${url})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        width: "100vw",
+        height: "100vh",
+      }}
+    >
       <div className="card w-96 bg-base-100 shadow-xl">
+        <figure>
+          <Image
+            src={`/${cityWeatherData.weather.current.weather[0].icon}.png`}
+            alt="icon"
+            width={400}
+            height={400}
+          ></Image>
+        </figure>
         <div className="card-body">
-          <h2 className="card-title"> {weatherData?.city}</h2>
-          <p>Text to display</p>
+          <h2 className="card-title">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
           <div className="card-actions justify-end">
             <button className="btn btn-primary">Buy Now</button>
           </div>
         </div>
       </div>
-    </div>
+    </main>
   );
 }
 
-const getWeather = async (
-  setWeatherData: React.Dispatch<SetStateAction<WeatherData | undefined>>,
-  getCoordinates: () => Promise<{ lat: number; lon: number }>
-): Promise<WeatherData> => {
-  const { environment } = nextConfig;
+const getCoordinates = async () => {
+  const {
+    coords: { latitude: lat, longitude: lon },
+  }: { coords: { latitude: number; longitude: number } } = await new Promise(
+    (res, rej) => {
+      navigator.geolocation.getCurrentPosition(res, rej);
+    }
+  );
+  return { lat, lon };
+};
 
+const fetchCityWeatherData = async (coords: Coords) => {
   const options = {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(await getCoordinates()),
+    body: JSON.stringify(coords),
   };
-
   try {
     const res = await fetch(`${environment}api/weather`, options);
-    const data = (await res.json()) as Promise<{
-      city: string;
-      weather: {} | undefined;
-    }>;
-    setWeatherData(await data);
+    const data = (await res.json()) as Promise<CityWeatherData>;
     return data;
   } catch (error) {
     throw error;
   }
 };
+
+export async function getServerSideProps() {
+  try {
+    const res = await fetch(`${environment}api/assets`);
+    const data = (await res.json()) as ServerSideAssets;
+    console.log(data);
+    return {
+      props: { data },
+    };
+  } catch (error) {
+    return {
+      props: { error },
+    };
+  }
+}
