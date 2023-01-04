e2cbbb2915a33adfd91c35b302989babd90a77ee
Ian <istreator@yahoo.com>
2023-01-04 03:04:25 -0600

    adjusted gradient color

--- components/QuoteCard.tsx
+++ components/QuoteCard.tsx
@@ -3,16 +3,16 @@ import { ServerSideAssets } from "../Types";
 import createTransformer from "tailwind-group-variant";
 const expandVariant = createTransformer();
 
-function QuoteCard({ quote: { quote, author } }: ServerSideAssets) {
+function QuoteCard({ quote }: ServerSideAssets) {
   return (
     <div
       className={expandVariant(
-        "w-screen text-white  py-4 flex flex-row justify-center"
+        "w-screen text-white  py-4 flex flex-row justify-center bg-gradient-to-b from-zinc-900"
       )}
     >
       <div className="w-10/12 px-4">
-        <p className="text-xl text-start">{quote}</p>
-        <p className="text-end pt-2">-{author}</p>
+        <p className="text-xl text-start">{quote.quote}</p>
+        <p className="text-end pt-2">-{quote.author}</p>
       </div>
     </div>
   );
