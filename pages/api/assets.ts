fa06968051d2e7d6579f10f2bad396b234e1fbb7
Ian <istreator@yahoo.com>
2023-01-04 03:06:11 -0600

    updated query to work with new database schema

--- pages/api/assets.ts
+++ pages/api/assets.ts
@@ -1,9 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import { Redis } from "@upstash/redis";
-import { ServerSideAssets, Quote, Error, Unsplash } from "../../Types";
-// const BASE_API_URL = "https://api.unsplash.com/";
-// const UNSPLASH_ACCESS_KEY = process.env.UNSPLASH_ACCESS_KEY;
+import {
+  ServerSideAssets,
+  Quote,
+  Error,
+  Unsplash,
+  RedisURL,
+} from "../../Types";
+const BASE_API_URL = "https://api.unsplash.com/";
+const UNSPLASH_ACCESS_KEY = process.env.UNSPLASH_ACCESS_KEY;
 const redis = Redis.fromEnv();
 
 export default async function handler(
@@ -12,12 +18,14 @@ export default async function handler(
 ) {
   try {
     // const unsplashRes = await fetch(
-    //   `${BASE_API_URL}search/photos?query=iceland&per_page=50&order_by=popular&client_id=${UNSPLASH_ACCESS_KEY}`
+    //   `${BASE_API_URL}search/photos?query=fungus&per_page=30&order_by=popular&client_id=${UNSPLASH_ACCESS_KEY}`
     // );
     // const { results } = await unsplashRes.json();
-    // const images = results.map(
-    //   (imageObject: Unsplash): string => imageObject.urls.full
-    // );
+    // const images = results.map((imageObject: Unsplash, i: number) => ({
+    //   i,
+    //   url: imageObject.urls.thumb,
+    // }));
+    // console.log(images);
     // const zen = "https://zenquotes.io/api/quotes/";
     // const zenRes = await fetch(zen);
     // const data = (await zenRes.json()) as { q: string; a: string }[];
@@ -27,14 +35,13 @@ export default async function handler(
     //     quote,
     //     author,
     //   }));
-    // await redis.lpush("quotes", ...filteredQuotes);
-    const randIndex = (length: number) => Math.floor(Math.random() * length);
+    // await redis.sadd("quotes2", ...filteredQuotes);
+    // await redis.rename("quotes2", "quotes");
+    // const randIndex = (length: number) => Math.floor(Math.random() * length);
 
-    const urlsLen = await redis.llen("urls");
-    const quotesLen = await redis.llen("quotes");
-
-    const url = (await redis.lindex("urls", randIndex(urlsLen))) as string;
-    const quote = (await redis.lindex("quotes", randIndex(quotesLen))) as Quote;
+    const randBackground = (await redis.srandmember("urls")) as RedisURL;
+    const url = randBackground[1].full;
+    const quote = (await redis.srandmember("quotes")) as Quote;
 
     res.status(200).json({ url, quote });
   } catch (error) {
