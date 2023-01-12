97e9762a592253e3e581cf4511b3a4e2468c2e5d
Ian <istreator@yahoo.com>
2023-01-12 00:54:18 -0600

    graph now displays percentage per hour and is based on the next 24 hours, new design

--- components/RainGraph.tsx
+++ components/RainGraph.tsx
@@ -1,57 +1,56 @@
 import React, { useEffect } from "react";
-import { Weather } from "../Types";
 import useTime from "../hooks/useTime";
-import useInches from "../hooks/useInches";
+import { Weather } from "../Types";
 
 function RainGraph({ data: { hourly } }: { data: Weather }) {
-  const first = hourly[0].dt;
-  const last = hourly[23].dt;
-
-  const { timeString: firstHour, timeAbbr: firstAbbr } = useTime(first);
-  const { timeString: lastHour, timeAbbr: lastAbbr } = useTime(last);
   useEffect(() => {
     const canvas = document.getElementById("canvas") as HTMLCanvasElement;
-    const ctx = canvas.getContext("2d");
+    const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 
     const width = (canvas.width = 400);
-    const origin = (canvas.height = 200);
+    const height = (canvas.height = 200);
+    const originY = height;
 
-    const graphHour = width / 47;
-    let graphTime = 0;
-
-    ctx?.beginPath();
-    ctx!.strokeStyle = "hsl(200,100%,75%)";
-    ctx!.lineWidth = 2.5;
-    ctx!.font = "35px Arial";
-    ctx!.textAlign = "center";
-    ctx?.moveTo(0, origin);
-    hourly?.forEach((hour, i) => {
-      ctx?.lineTo(graphTime, origin - hour.pop * 50);
-      if (i === hourly.length - 1) return;
-      if (i === 55 || i === 5) {
-        ctx!.fillStyle = "white";
-        ctx?.fillText(`${hour.pop}%`, graphTime, origin - hour.pop * 50 - 10);
+    const nextDayIndex = hourly.findIndex(({ dt }, i) => {
+      const { hour, timeAbbr } = useTime(dt);
+      if (timeAbbr === "AM" && hour === 12) {
+        return i;
       }
+    });
+    const graphHour = width / 24;
+    let graphTime = 0;
+    ctx.lineWidth = 5;
+    ctx.font = "35px Gill Sans";
+    ctx.textAlign = "center";
 
+    hourly?.forEach(({ pop, dt }, i) => {
+      const popScaled = pop * 200;
+      if (i > 24) return;
+      ctx.fillStyle = "hsla(200,100%,75%,0.5)";
+      ctx.strokeStyle = "hsl(200,100%,75%)";
+
+      if (i > nextDayIndex) {
+        ctx.strokeStyle = "hsla(0,100%,100%,0)";
+        ctx.fillStyle = "hsla(200,100%,75%,0.25)";
+      }
+      ctx.fillRect(graphTime, originY - popScaled, graphHour, popScaled);
+      ctx.lineWidth = 2.5;
+
+      ctx.strokeRect(graphTime, originY - popScaled, graphHour, popScaled);
       graphTime += graphHour;
     });
-    ctx?.lineTo(graphTime, origin);
-    ctx?.closePath();
-    ctx!.fillStyle = "hsla(200,100%,75%,0.25)";
-    ctx?.fill();
-    ctx?.stroke();
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = "hsl(0,0%,100%)";
+    ctx.strokeRect(0, 0, width, height);
+    ctx.fill();
   }, [hourly]);
 
   return (
     <div className="w-10/12 mx-auto my-2">
-      <h1 className="text-sm text-start font-bold">Precipitation(%)</h1>
-      {/* <div className="w-full flex justify-between">
-        <p className="text-xs  text-start">{firstHour + " " + firstAbbr}</p>
-        <p className="text-xs  text-start">{lastHour + " " + lastAbbr}</p>
-      </div> */}
-      <div className="canvas-container bg-gradient-to-tr from-cyan-900/10 to-cyan-900/30">
-        <canvas id="canvas" className="w-full"></canvas>
-      </div>
+      <h1 className="text-xs text-start font-bold">
+        <span>24/Hr</span> Chance of rain
+      </h1>
+      <canvas id="canvas" className="w-full"></canvas>
     </div>
   );
 }
