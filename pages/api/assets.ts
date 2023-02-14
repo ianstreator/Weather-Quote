76dbc8a3914079d7c86e5333df6f735ff6959e09
Ian <istreator@yahoo.com>
2023-02-13 20:37:16 -0600

    cleaned up file

--- pages/api/assets.ts
+++ pages/api/assets.ts
@@ -2,8 +2,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { Redis } from "@upstash/redis";
 import { ServerSideAssets, Quote, Error, URLs } from "../../Types";
-const BASE_API_URL = "https://api.unsplash.com/";
-const UNSPLASH_ACCESS_KEY = process.env.UNSPLASH_ACCESS_KEY;
 const redis = Redis.fromEnv();
 
 export default async function handler(
@@ -11,28 +9,6 @@ export default async function handler(
   res: NextApiResponse<ServerSideAssets | Error>
 ) {
   try {
-    // const unsplashRes = await fetch(
-    //   `${BASE_API_URL}search/photos?query=fungus&per_page=30&order_by=popular&client_id=${UNSPLASH_ACCESS_KEY}`
-    // );
-    // const { results } = await unsplashRes.json();
-    // const images = results.map((imageObject: Unsplash, i: number) => ({
-    //   i,
-    //   url: imageObject.urls.thumb,
-    // }));
-    // console.log(images);
-    // const zen = "https://zenquotes.io/api/quotes/";
-    // const zenRes = await fetch(zen);
-    // const data = (await zenRes.json()) as { q: string; a: string }[];
-    // const filteredQuotes = data
-    //   .filter(({ q }) => q.length < 71)
-    //   .map(({ q: quote, a: author }) => ({
-    //     quote,
-    //     author,
-    //   }));
-    // await redis.sadd("quotes2", ...filteredQuotes);
-    // await redis.rename("quotes2", "quotes");
-    // const randIndex = (length: number) => Math.floor(Math.random() * length);
-
     const randBackground = (await redis.srandmember("urls")) as URLs;
     const url = randBackground[1].full;
     const quote = (await redis.srandmember("quotes")) as Quote;
