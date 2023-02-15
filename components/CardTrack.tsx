11baf4e71b92e5b9b1dd37b6268c9ebd02dcae5d
Ian <istreator@yahoo.com>
2023-02-14 23:32:35 -0600

    styles

--- components/CardTrack.tsx
+++ components/CardTrack.tsx
@@ -5,7 +5,7 @@ import WeekDayCard from "./WeekDayCard";
 
 function CardTrack({ data }: { data: WeekdayData[] }) {
   return (
-    <div className="scrollbar overflow-y-hidden flex max-w-xs m-auto mb-0 space-x-8 p-0 bg-transparent md:max-w-full md:w-full md:justify-between">
+    <div className="scrollbar overflow-y-hidden flex max-w-xs mx-auto mt-4 space-x-8 p-0 bg-transparent md:max-w-full md:w-full md:justify-between">
       {data.map((day, i) => {
         return <WeekDayCard key={i} data={day} />;
       })}
