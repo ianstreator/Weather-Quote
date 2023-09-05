5e5a5f4ed3ee651ffa3cb156c88734801cd185eb
Ian <ianstreator+github@gmail.com>
2023-09-05 17:59:36 -0500

    broke down weekday card into two separate components and brought them in

--- components/WeekDayCard.tsx
+++ components/WeekDayCard.tsx
@@ -1,145 +1,42 @@
 import React, { useState } from "react";
 import clsx from "clsx";
-import Image from "next/image";
-import { Daily } from "../Types";
-import useGetDay from "../hooks/useGetDay";
-import useInches from "../hooks/useInches";
-import useTime from "../hooks/useTime";
+import { Daily } from "../types";
 
-const iconSize = 30;
+import FrontWeatherCard from "./FrontWeatherCard";
+import BackWeatherCard from "./BackWeatherCard";
 
 function WeekDayCard({
-  dt,
+  dt: timeEpoch,
   rain,
   snow,
   sunrise,
   sunset,
-  temp: { day, min, max },
+  temp: { day: avgTemp, min: minTemp, max: maxTemp },
   weather: {
-    0: { description, icon },
+    0: { description: weatherDescription, icon: weatherIcon },
   },
-  pop,
+  pop: chanceOfPrecipitation,
 }: Daily) {
   const [cardFlipped, setCardFlipped] = useState<boolean>(false);
-
-  const { timeString: riseTime, timeAbbr: riseAbbr } = useTime(sunrise);
-  const { timeString: setTime, timeAbbr: setAbbr } = useTime(sunset);
-
-  const rainInches = useInches(rain)
-  const snowInches = useInches(snow)
-
-  const turnArrow = (
-    <figure className="arrow">
-      <Image
-        src="/turn-arrow.svg"
-        alt="weather-icon"
-        width={iconSize}
-        height={iconSize}
-      />
-    </figure>
-  );
-
-  const front = (
-    <div className="card front">
-      <h1>{useGetDay(dt)}</h1>
-      <h1>{`${Math.round(day)}°`}</h1>
-
-      <figure className="">
-        <Image
-          src={`/${icon}.svg`}
-          alt="weather-icon"
-          width={iconSize}
-          height={iconSize}
-        />
-      </figure>
-      <p>{description}</p>
-      {turnArrow}
-    </div>
-  );
-
-  const back = (
-    <div className="card back text-sm">
-      <fieldset>
-        <legend>Temperature</legend>
-        <div className="w-11/12 flex justify-between items-center mx-auto">
-          <p>{`H: ${Math.round(max)}°`}</p>
-          <Image
-            src={"/temp-diff.svg"}
-            alt="temp-diff"
-            width={iconSize}
-            height={iconSize}
-          ></Image>
-          <p className="opacity-60">{`L: ${Math.round(min)}°`}</p>
-        </div>
-      </fieldset>
-
-      <fieldset>
-        <legend>
-          Precipitation{" "}
-          <span className="text-xs font-thin">{(pop * 100).toFixed(0)}%</span>
-        </legend>
-
-        {rain && description.includes("rain") && (
-          <div className="flex items-center m-auto ">
-            <Image
-              src={"/rain-drop.svg"}
-              alt="rain"
-              width={iconSize}
-              height={iconSize}
-            ></Image>
-
-            <p>{rainInches}</p>
-          </div>
-        )}
-        {snow && description.includes("snow") && (
-          <div className="flex items-center m-auto ">
-            <Image
-              src={"/snow-flake.svg"}
-              alt="snow"
-              width={iconSize}
-              height={iconSize}
-            ></Image>
-
-            <p>{snowInches}</p>
-          </div>
-        )}
-      </fieldset>
-      <fieldset>
-        <legend>Sun</legend>
-        <div className="relative w-11/12 flex flex-row justify-between mx-auto">
-          <div className="absolute top-0 left-0 text-start">
-            <p> {riseTime}</p>
-            <p className="text-xs"> {riseAbbr}</p>
-          </div>
-
-          <Image
-            src={"/sun-rise-set.svg"}
-            width={iconSize * 1.5}
-            height={iconSize * 1.5}
-            alt="sun-rise-set"
-            className="mt-auto mx-auto"
-          ></Image>
-
-          <div className="absolute bottom-0 right-0 text-end">
-            <p className="text-xs"> {setAbbr}</p>
-            <p> {setTime}</p>
-          </div>
-        </div>
-      </fieldset>
-
-      {turnArrow}
-    </div>
-  );
+  const frontCardData = { timeEpoch, avgTemp, weatherDescription, weatherIcon };
+  const backCardData = {
+    maxTemp,
+    minTemp,
+    rain,
+    snow,
+    weatherDescription,
+    sunrise,
+    sunset,
+    chanceOfPrecipitation,
+  };
 
   return (
-    <div className="carousel-item">
-      <div
-        onClick={() => setCardFlipped(!cardFlipped)}
-        className={clsx("weekday-card", cardFlipped && "flipped")}
-      >
-        {front}
-        {back}
-      </div>
+    <div
+      onClick={() => setCardFlipped(!cardFlipped)}
+      className={clsx("custom-card w-36 h-60 relative ml-8 first:ml-0", cardFlipped && "flipped")}
+    >
+      <FrontWeatherCard {...frontCardData} />
+      <BackWeatherCard {...backCardData} />
     </div>
   );
 }
